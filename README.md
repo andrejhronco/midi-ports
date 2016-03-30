eb77de9a24ebacb4482cd6dc722ca2fba8c900c6
AndrejHronco <andrej@andrej-kmi.local>
2016-03-30 13:44:48 -0700

    push midi-ports

---
+++ README.md
@@ -0,0 +1,84 @@
+#midi-ports
+
+Returns an object of attached midi ports with inputID, outputID, name, and manufactuer for more symantic MIDI port access.
+
+##install
+```javascript
+npm install midi-ports --save
+```
+##usage
+```javascript
+import midiPorts from 'midi-ports'
+
+let ports = midiPorts(midi, [options])
+/*
+=> 
+{
+	'k-mix-audio-control': {
+		name: 'K-Mix Audio Control',
+		inputId: '-543473823',
+		outputId: '586923498',
+		manufacturer: 'Keith McMillen Instruments'
+	},
+	'k-mix-control-surface': {
+		name: 'K-Mix Control Surface',
+		inputId: '-543345892',
+		outputId: '654298746',
+		manufacturer: 'Keith McMillen Instruments'
+	}
+}
+*/
+```
+Which enables port access by name:
+
+```javascript
+let kmixInput = midi.inputs.get(ports['k-mix-control-surface'].inputID)
+kmixInput.onmidimessage = (e) => console.log('data', e.data)
+
+// or
+
+let kmixOutput = midi.outputs.get(ports['k-mix-audio-control'].outputID)
+kmixOutput.send([176, 1, 64])
+```
+
+##Allowed Devices
+An optional object can be passed in to only gather data of desired devices and midi ports. All other attached devices and  ports will be ignored. 
+
+This is especially useful if multiple MIDI devices are attached to the system and you want easy access to each port.
+
+```javascript
+let allowed = {
+	'k-mix': {
+		'k-mix-audio-control': { },
+		'k-mix-control-surface': { },
+		'icon': 'https://files.keithmcmillen.com/products/k-mix/icons/k-mix.svg',
+		'manufacturer': 'Keith McMillen Instruments'
+	}
+}
+
+let devices = midiPorts(midi, allowed)
+/*
+ => 
+ {
+	'k-mix': {
+		'k-mix-audio-control': {
+			name: 'K-Mix Audio Control',
+			inputID: '-543473823',
+			outputID: '586923498',
+			manufacturer: 'Keith McMillen Instruments'
+		},
+		'k-mix-control-surface': {
+			'name': 'K-Mix Control Surface',
+			inputID: '-543345892',
+			outputID: '654298746',
+			manufacturer: 'Keith McMillen Instruments'
+		},
+		'icon': 'https://files.keithmcmillen.com/products/k-mix/icons/k-mix.svg',
+		'manufacturer': 'Keith McMillen Instruments'
+	}
+}
+*/
+
+let kmixOutput = midi.outputs.get(devices['k-mix']['k-mix-audio-control'].outputID)
+
+kmixOutput.send([240, 126, 127, 6, 1, 247])
---
+++ index.js
@@ -0,0 +1,86 @@
+/**
+ * MIDI device port (input & output) properties [name, ID, manufacturer]
+ * @function
+ * @param {Object} midi - MIDI access object
+ * @returns {Object} Port map of device properties' port (input & output) properties [name, ID, manufacturer]
+ */
+function getPorts(midi){
+	let portMap = {}
+	
+	midi.inputs.forEach(function(device){
+		portMap[format(device.name)] = { 
+			'name': device.name, 
+			'inputID': format(device.id), 
+			'manufacturer': format(device.manufacturer) }
+	})
+
+	midi.outputs.forEach(function(device){
+		portMap[format(device.name)]['outputID'] = format(device.id)
+	})
+
+	return portMap;
+}
+
+/**
+ * MIDI device ports allowed for this App.
+ * @function
+ * @param {Array} deviceNames - array of MIDI devices derived from source
+ * @param {Object} ports - Port map of device properties
+ * @param {Object} source - chosen MIDI device list
+ * @returns {Object} devices with port data
+ */
+function buildDevices(deviceNames, ports, source){
+	let props = ['name', 'inputID', 'outputID', 'manufacturer']
+
+	if(!Object.keys(source).length) source = Object.assign({}, ports)
+	/**
+	 * @var {Array} devices - copied source devices
+	 */
+	let devices = Object.assign({}, source)
+
+	deviceNames.forEach(function(device){
+		Object.keys(ports).forEach(function(port){
+			if(devices[device][port]) {
+				props.forEach(function(prop){
+					devices[device][port][prop] = ports[port][prop]
+				})
+			}
+		})
+	})
+
+	return devices
+}
+
+/**
+ * Final MIDI devices object with all port (input & output) properties [name, ID, manufacturer]
+ * @function
+ * @param {Object} midi - MIDI access object
+ * @param {Object} sourceDevices - initial MIDI devices
+ * @returns {Object} All MIDI devices with port [name, ID, manufacturer] properties
+ */
+function midiPorts(midi, source = {}){
+	/**
+	* @var {Array} deviceNames - desired device names
+	*/ 
+	let deviceNames = Object.keys(source)
+
+	/**
+	* @var {Object} ports - device ports (input & output) with properties [name, ID, manufacturer]
+	*/ 
+	let ports = getPorts(midi)
+	
+	/**
+	* @var {Object} devices - All MIDI devices with port [name, ID, manufacturer] properties
+	*/ 
+	let devices = buildDevices(deviceNames, ports, source)
+
+	return devices;
+}
+
+function format(string){
+	return string.toLowerCase().replace(/\s/g, '-').replace(',','')
+}
+
+//export { midiPorts as default };
+
+module.exports = midiPorts
