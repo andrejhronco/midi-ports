aacfdc177744bc4fd2c89727b65d0183315cfb25
AndrejHronco <andrej@andrej-kmi.local>
2016-08-02 17:43:14 -0700

    Version 2
    midi-ports now wraps the Web MIDI Access object to make accessing ports
    by name much cleaner.

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 #midi-ports
-
-Returns an object of attached **web midi** ports with inputID, outputID, name, and manufactuer for more symantic MIDI port access.
+Builds an object of MIDI devices with name, inputID, outputID, and manufacturer and wraps the MIDI Access object to allow a more symantic way of interacting with MIDI devices.
 
 ##install
 ```javascript
@@ -12,13 +11,15 @@ import midiPorts from 'midi-ports'
 
 let midi, ports;
 
-navigator.requestMIDIAccess({sysex: true})
+navigator.requestMIDIAccess({sysex: true}) // set to true if you need to send sysex messages
 	.then((midiAccess) => {
 		midi = midiAccess
 		ports = midiPorts(midi [,options])
 	})
 /*
-ports =>
+internally builds a devices/ports object:
+
+ports('devices') =>
 {
 	'k-mix-audio-control': {
 		name: 'K-Mix Audio Control',
@@ -38,19 +39,32 @@ ports =>
 Which enables port access by name:
 
 ```javascript
-let kmixInput = midi.inputs.get(ports['k-mix-control-surface'].inputID)
+// get an input and listen for messsages
+let kmixInput = ports('k-mix-control-surface').get('input')
 kmixInput.onmidimessage = (e) => console.log('data', e.data)
 
-// or
-
-let kmixOutput = midi.outputs.get(ports['k-mix-audio-control'].outputID)
+// or, get an output and send messages
+let kmixOutput = ports('k-mix-control-surface').get('output')
 kmixOutput.send([176, 1, 64])
 ```
 
 ##Allowed Devices
-An optional object can be passed in to only gather data of desired devices and midi ports. All other attached devices and  ports will be ignored. 
+An optional object can be passed in to only gather data of desired devices and midi ports. All other attached devices and ports will be ignored. * see error handling below
 
-This is especially useful if multiple MIDI devices are attached to the system and you want easy access to each port.
+This is especially useful if multiple MIDI devices are attached to the system and you want easy access to a specific port.
+
+The allowed devices object should be formated:
+
+```javascript
+let allowed = {
+	'device': {
+		'port-name': { }, // empty object which gets populated with name, inputID, outputID, and manufacturer
+		'other-info': 'this can be any info you want to reference'
+	}
+}
+```
+
+*Port names must be lowercase and hyphen-separated.
 
 ```javascript
 let allowed = {
@@ -59,53 +73,118 @@ let allowed = {
 		'k-mix-control-surface': { },
 		'icon': 'https://files.keithmcmillen.com/products/k-mix/icons/k-mix.svg',
 		'manufacturer': 'Keith McMillen Instruments'
+	},
+	'k-board':{
+		'k-board': { },
+		'icon': 'https://files.keithmcmillen.com/products/k-board/icons/k-board.svg',
+		'manufacturer': 'Keith McMillen Instruments'
 	}
 }
-
+//...
 let devices = midiPorts(midi, allowed)
 /*
- => 
+internally builds a devices.ports object:
+
+ports('devices') => 
  {
 	'k-mix': {
 		'k-mix-audio-control': {
 			name: 'K-Mix Audio Control',
 			inputID: '-543473823',
 			outputID: '586923498',
-			manufacturer: 'Keith McMillen Instruments'
+			manufacturer: 'keith-mcmillen-instruments'
 		},
 		'k-mix-control-surface': {
 			'name': 'K-Mix Control Surface',
 			inputID: '-543345892',
 			outputID: '654298746',
-			manufacturer: 'Keith McMillen Instruments'
+			manufacturer: 'keith-mcmillen-instruments'
 		},
 		'icon': 'https://files.keithmcmillen.com/products/k-mix/icons/k-mix.svg',
+		'manufacturer': 'Keith McMillen Instruments'
+	},
+	'k-board':{
+		'k-board': {
+			name:'K-Board',
+			inputID:'1852960744',
+			outputID:'-162522465',
+			manufacturer:'kesumo-llc'
+		},
+		'icon': 'https://files.keithmcmillen.com/products/k-board/icons/k-board.svg',
 		'manufacturer': 'Keith McMillen Instruments'
 	}
 }
 */
+```
+To access grouped devices, use the 'device:port-name' format 
 
-let kmixOutput = midi.outputs.get(devices['k-mix']['k-mix-audio-control'].outputID)
+```javascript
+let kmixOutput = devices('k-mix:k-mix-audio-control').get('output')
 
 kmixOutput.send([240, 126, 127, 6, 1, 247])
 ```
-##Error Handling
-New in 1.1.0.
-If you're passing in an 'allowed devices' object and that device is not connected/found, midi-ports will add a property called 'notfound' populated with not found device names allowing for easy error handlng. Additionally, a 'ports' property with _*all*_ connected ports will be added as well, allowing you setup fallback ports if desired.
+_*Grouped Devices*_
+If a device in your allowed devices object only has one port that has the same name as your device, you can use a shorthand for getting that port.
 
-In the case above, if 'k-mix' is not connected/found, midi-ports will return an object like this:
+```javascript
+	// you can use either
+	devices('k-board:k-board').get('input') // => MIDIInput
+	// or, the shorter
+	devices('k-board').get('input') // => MIDIInput
+```
+
+##Accessing MIDIAccess and The Device Object
+You can also get direct access to the MIDIAccess object, including 'inputs' and 'outputs', and device object.
+
+```javascript
+//...
+let devices = midiPorts(midi, allowed)
+// get device object, a la midi-ports v.1.x
+devices('devices') // => {'device': 'port-name': { ... }}
+// get MIDI Access object
+devices('access') // => MIDIAccess
+// get MIDI Access inputs / outputs iterator
+// ** when getting inputs / outputs from the MIDI Access object you must use the 'midi' param
+devices('midi').get('inputs') // => MIDIInputMap
+// if using allowed devices object and a device isn't found ** see Error Handling below
+devices('notfound') // => array of notfound ports ['k-mix-audio-control','k-mix-control-surface']
+```
+
+##Setting / Getting data
+You can also set arbitrary port-specific data using the set method. Set and Get can be chained.
+
+```javascript
+let devices = midiPorts(midi)
+devices('k-board').set('quality', 'great!')
+// get custom data
+devices('k-board').get('quality') // => great!
+// chain set and get
+devices('k-board').set('price', '$99').set('review', 'awesome!').get('review') // => 'awesome!'
+```
+
+##Error Handling
+
+If you're passing in an 'allowed devices' object and that device is not connected/found, midi-ports will add a each not-found port to an internal list, allowing for easy error handlng. Additionally, a 'ports' property with _*all*_ connected ports will be added as well, allowing you setup fallback ports if desired.
+
+In the case above, if 'k-mix' is not connected/found, querying 'devices' will return an object like this:
 
 ```javascript
 /*
-ports =>
+ports('devices') =>
 {
-	'notfound': ['k-mix'],
+	'notfound': ['k-mix-audio-control','k-mix-control-surface'],
+ 	'k-board': {
+   	 	name: "K-Board",
+    	inputID: "1852960744",
+    	outputID: "-162522465",
+    	manufacturer: "kesumo-llc"
+	},
 	'ports: {
 		'k-board': {
-			inputID: "1852960744",
-			manufacturer: "kesumo-llc",
 			name: "K-Board",
-			outputID: "-162522465"
+			inputID: "1852960744",
+			outputID: "-162522465",
+			manufacturer: "kesumo-llc"
 		}
 	}
 }
@@ -114,10 +193,10 @@ ports =>
 Which makes it easy to use alternative ports if your desired port isn't connected/found:
 
 ```javascript
-if(ports['notfound']){
-	console.log('Device ' + ports['notfound'][0] + ' not found)
+if(ports('notfound').length){
+	console.log('Device ' + ...ports('notfound') + ' not found)
 	
-	// use a port from ports[ports]
-	...
+	// use an alternate port from ports('devices').ports
+	// ...
 }
 ```
--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-/**
- * MIDI device port (input & output) properties [name, ID, manufacturer]
- * @function
- * @param {Object} midi - MIDI access object
- * @returns {Object} Port map of device properties' port (input & output) properties [name, ID, manufacturer]
- */
-function getPorts(midi){
-	var portMap = {}
-	
-	midi.inputs.forEach(function(device){
-		portMap[format(device.name)] = { 
-			'name': device.name, 
-			'inputID': format(device.id), 
-			'manufacturer': format(device.manufacturer) }
-	})
+  /**
+   * MIDI device port (input & output) properties [name, ID, manufacturer]
+   * @function
+   * @param {Object} midi - MIDI access object
+   * @returns {Object} Port map of device properties' port (input & output) properties [name, ID, manufacturer]
+   */
+function getPorts(midi) {
+  let portMap = {}
 
-	midi.outputs.forEach(function(device){
-		if(portMap[format(device.name)]) portMap[format(device.name)]['outputID'] = format(device.id)
-	})
+  midi.inputs.forEach(function(device) {
+    portMap[format(device.name)] = {
+      'name': device.name,
+      'inputID': format(device.id),
+      'manufacturer': format(device.manufacturer)
+    }
+  })
 
-	return portMap;
+  midi.outputs.forEach(function(device) {
+    if (portMap[format(device.name)]) portMap[format(device.name)]['outputID'] = format(device.id)
+  })
+
+  return portMap;
 }
 
 /**
@@ -29,78 +30,206 @@ function getPorts(midi){
  * @param {Object} source - chosen MIDI device list
  * @returns {Object} devices with port data
  */
-function buildDevices(deviceNames, ports, source){
-	var props = ['name', 'inputID', 'outputID', 'manufacturer']
+function buildDevices(deviceNames, ports, source) {
+  let props = ['name', 'inputID', 'outputID', 'manufacturer']
 
-	if(!Object.keys(source).length) source = Object.assign({}, ports)
-	/**
-	 * @var {Array} devices - copied source devices
-	 */
-	var devices = Object.assign({}, source)
+  if (!Object.keys(source).length) source = Object.assign({}, ports)
+    /**
+     * @let {Array} devices - copied source devices
+     */
+  let devices = Object.assign({}, source)
 
-	devices['notfound'] = []
-	// build devices
-	deviceNames.forEach(function(device){
-		Object.keys(ports).forEach(function(port){
-			if(devices[device][port]) {
-				props.forEach(function(prop){
-					devices[device][port][prop] = ports[port][prop]
-				})
-			}
-		})
-		// populate notfound
-		Object.keys(devices[device]).forEach(function(port) {
-		  if (!Object.keys(devices[device][port]).length) {
-		    devices['notfound'].push(device)
-		  }
-		})
-	})
+  devices.notfound = []
+  // build devices
+  deviceNames.forEach(function(device) {
+    Object.keys(ports).forEach(function(port) {
+        if (devices[device][port]) {
+          props.forEach(function(prop) {
+            devices[device][port][prop] = ports[port][prop]
+          })
+        }
+      })
+    // populate notfound with port
+    Object.keys(devices[device]).forEach(function(port) {
+      if (!Object.keys(devices[device][port]).length) {
+        devices.notfound.push(port)
+      }
+    })
+  })
+  // remove notfound ports
+  if (devices.notfound.length) {
+    Object.keys(devices).forEach(function(device) {
+    	if(device !== 'notfound'){
+	    	Object.keys(devices[device]).forEach(function(port){
+        	if(devices.notfound.includes(port)) delete devices[device]
+      	})
+      }  
+    })
+    // if a device is not found add all ports
+    devices.ports = ports
+  } else {
+    delete devices.notfound
+  }
 
-	// remove notfound devices
-	if (devices['notfound'].length) {
-	  devices['notfound'].forEach(function(device) {
-	    delete devices[device]
-	  })
-	  // if a device is not found add all ports
-	  devices['ports'] = ports
-	} else {
-	  delete devices['notfound']
-	}
-
-	return devices
+  return devices
 }
 
 /**
- * Final MIDI devices object with all port (input & output) properties [name, ID, manufacturer]
+ * Builds MIDI devices object with all port (input & output) properties [name, ID, manufacturer]
  * @function
  * @param {Object} midi - MIDI access object
  * @param {Object} sourceDevices - initial MIDI devices
- * @returns {Object} All MIDI devices with port [name, ID, manufacturer] properties
+ * @returns {Function} Allowing access to midi access object, ports, device object, and notfound
  */
-function midiPorts(midi, source){
-	source = source || {}
-	/**
-	* @var {Array} deviceNames - desired device names
-	*/ 
-	var deviceNames = Object.keys(source)
+function midiPorts(midi, source) {
+  source = source || {}
+    /**
+     * @let {Array} deviceNames - desired device names
+     */
+  let deviceNames = Object.keys(source)
 
-	/**
-	* @var {Object} ports - device ports (input & output) with properties [name, ID, manufacturer]
-	*/ 
-	var ports = getPorts(midi)
-	
-	/**
-	* @var {Object} devices - All MIDI devices with port [name, ID, manufacturer] properties
-	*/ 
-	var devices = buildDevices(deviceNames, ports, source)
+  /**
+   * @let {Object} ports - device ports (input & output) with properties [name, ID, manufacturer]
+   */
+  let ports = getPorts(midi)
 
-	return devices;
+  /**
+   * @let {Object} devices - All MIDI devices with port [name, ID, manufacturer] properties
+   */
+  let devices = buildDevices(deviceNames, ports, source)
+
+  devices.access = midi
+	/**
+	* Returns midi access object, ports, device object, and notfound
+	* @Function
+	* param {String} 'access', 'midi', 'notfound', or 'device:port' to get props from
+	* @example
+	* // returns device object
+	* ports('devices')
+	* @example
+	* // returns full midi access object
+	* ports('access')
+	* @example
+	* // returns midi inputs Iterator
+	* ports('midi').get('inputs')
+	* @example
+	* // returns Boolean if desired / allowed ports are not found
+	* ports('notfound')
+	*/
+  return function(device = 'midi') {
+    // tests
+    let isAccess = (device === 'access'.toLowerCase()) ? true : false,
+        isMIDI = (device === 'midi'.toLowerCase()) ? true : false,
+        isDevices = (device === 'devices'.toLowerCase()) ? true : false,
+        isNotFound = (device === 'notfound'.toLowerCase()) ? true : false,
+        notFound = (devices['notfound'] && devices['notfound'].length) ? true : false,
+        isDevicePort = (!isAccess && device.toLowerCase().includes(':')) ? true : false,
+        inDevices = (device in devices) ? true : false;
+    // device:port
+    let dvc = (isDevicePort) ? device.split(':') : device,
+        inDevicesPort = (isDevicePort && dvc[1] in devices[dvc[0]]) ? true : false;
+
+    if (isDevices) {
+      let devicesNoAccess = Object.assign({}, devices)
+      delete devicesNoAccess.access
+      return devicesNoAccess
+    } else if (isAccess) {
+      return devices.access
+    } else if(isNotFound){
+      if(notFound){
+        return devices['notfound']
+      } else {
+        return false
+      }
+    }
+    /**
+    * Returns object with get and set methods, set supports chaining
+    * @Object
+    */
+    return {
+      /**
+      * param {String} property from device, midi, or device object 
+      * @example
+      * // returns device input object
+      * ports('k-board').get('input')
+      * @example
+      * // sets property on device object
+      * ports('k-board').set('quality', 'great!')
+      * @example
+      * // returns property value [name, manufacturer, custom]
+      * ports('k-board').get('quality'))
+      */
+      get: function get(property) {
+        if (!property) return
+
+        let prop,
+            isIO = (property.includes('input') || property.includes('output')),
+            inputType = (isIO && property.includes('input')) ? 'inputs' : 'outputs',
+            suffix = (isIO && !isMIDI) ? 'ID' : '';
+        
+        if (isMIDI && !!devices.access[property]) {
+          prop = devices.access[property]
+        } else if (isDevicePort && inDevicesPort) { // device:port
+          if (isIO) {
+            prop = devices.access[inputType].get(devices[dvc[0]][dvc[1]][property + suffix])
+          } else {
+            prop = devices[dvc[0]][dvc[1]][property]
+          }
+        } else if (!isDevicePort && inDevices) { // 'port'
+          if (isIO) {
+            if (!!devices[dvc][dvc]) { // device:device exists
+              prop = devices.access[inputType].get(devices[dvc][dvc][property + suffix])
+            } else {
+              prop = devices.access[inputType].get(devices[dvc][property + suffix])
+            }
+          } else {
+            if (!!devices[dvc][dvc]) {
+              prop = devices[dvc][dvc][property]
+            } else {
+              prop = devices[dvc][property]
+            }
+          }
+        } else {
+          console.warn('port '+ device +' not found')
+        }
+        return prop
+      },
+      /**
+      * param {String} property to be set
+      * param {String} property value to be set
+      * @example
+      * // sets property on device object
+      * ports('k-board').set('quality', 'great!')
+      * @example
+      * // returns property value [name, manufacturer, custom]
+      * ports('k-board').get('quality'))
+      */
+      set: function set(property, value) {
+        if(isDevicePort && !!devices[dvc[0]][dvc[1]]){
+          devices[dvc[0]][dvc[1]][property] = value
+        } else if(inDevices){
+          if(!!devices[device][device]){
+            devices[device][device][property] = value
+          } else {
+            devices[device][property] = value
+          }
+        } else {
+          console.warn('port '+ device +' not found')
+        }
+				return this
+      }
+    }
+  }
 }
 
-function format(string){
-	return string.toLowerCase().replace(/\s/g, '-').replace(',','')
+/**
+ * Formats string to remove spaces and convert to lowercase
+ * @function
+ * @param {String} string to format
+ * @returns {String} formatted string
+ */
+function format(string) {
+  return string.toLowerCase().replace(/\s/g, '-').replace(',', '')
 }
-
-//export { midiPorts as default };
 
 module.exports = midiPorts
